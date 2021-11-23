7c4ea0a6e774db5c209862e4898891fd836736b8
Bryan Jiencke <bryan.jiencke@gmail.com>
2021-11-23 17:22:35 -0500

    WIP

--- bin/cloud-provider-bridge.sh
+++ bin/cloud-provider-bridge.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # gcloud beta container --project "bdbd-310322" clusters create "sys" --zone "us-east4-c" --no-enable-basic-auth --cluster-version "1.20.9-gke.701" --release-channel "regular" --machine-type "e2-standard-4" --image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true --service-account "sys-admin@bdbd-310322.iam.gserviceaccount.com" --max-pods-per-node "110" --preemptible --num-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias --network "projects/bdbd-310322/global/networks/default" --subnetwork "projects/bdbd-310322/regions/us-east4/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --enable-autoscaling --min-nodes "0" --max-nodes "5" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --node-locations "us-east4-c"
 
 # gcloud beta container --project "bdbd-310322" clusters create "bd" --zone "us-east4-c" --no-enable-basic-auth --cluster-version "1.21.4-gke.301" --release-channel "rapid" --machine-type "e2-standard-4" --image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --max-pods-per-node "110" --preemptible --num-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-private-nodes --master-ipv4-cidr "172.16.0.0/28" --enable-ip-alias --network "projects/bdbd-310322/global/networks/sys1" --subnetwork "projects/bdbd-310322/regions/us-east4/subnetworks/subnet-us-east-192" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --enable-autoscaling --min-nodes "3" --max-nodes "5" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --node-locations "us-east4-c"
@@ -7,7 +9,7 @@
 # gcloud beta container --project "sysf-12" clusters create "cluster-1" --zone "us-east4-a" --no-enable-basic-auth --cluster-version "1.20.10-gke.301" --release-channel "regular" --machine-type "e2-medium" --image-type "COS_CONTAINERD" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --max-pods-per-node "110" --num-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias --network "projects/sysf-12/global/networks/sysf" --subnetwork "projects/sysf-12/regions/us-east4/subnetworks/us-east4-192" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --node-locations "us-east4-a"
 
 gcloud container clusters create sysf \
---cluster-version "1.21.3-gke.2001" \
+--cluster-version "1.21.5-gke.1302" \
 --no-enable-basic-auth \
 --no-enable-master-authorized-networks \
 --enable-ip-alias \
@@ -39,15 +41,15 @@ kubectl create secret generic ssh-config -n default --from-file=/Users/bry/sys/e
 kubectl create secret tls "tls-sys" -n default --cert=/etc/letsencrypt/live/sysf.one/fullchain.pem --key=/etc/letsencrypt/live/sysf.one/privkey.pem
 
 kubectl create namespace admin
-kubectl create secret generic config-grafana -n admin --from-file=/Users/bry/sys/etc/grafana.ini 
+kubectl create secret generic config-grafana -n admin --from-file=/Users/bry/sys/etc/grafana.ini
 kubectl get secret tls-sys --namespace=default -o yaml | sed 's/namespace: default/namespace: admin/' | kubectl apply --namespace=admin -f - || true
 
-cd /Users/bry/sys/kubernetes/cluster/sysf/k8s 
+cd /Users/bry/sys/kubernetes/cluster/sysf/k8s
 kubectl apply -k ./
 cd -
 
 cd /Users/bry/sys/kubernetes/cluster/sysf/applications/rook-ceph/rook-ceph
-helmfile --environment=rook-ceph apply 
+helmfile --environment=rook-ceph apply
 cd -
 
 sleep 90
@@ -56,9 +58,9 @@ cd /Users/bry/sys/kubernetes/cluster/sysf/applications/rook-ceph/rook-ceph-clust
 helmfile --environment=rook-ceph apply
 cd -
 
-kubectl create namespace k10
-kubectl apply -f /Users/bry/sys/etc/k10-oidc-auth.yml
-kubectl get secret tls-sys --namespace=default -o yaml | sed 's/namespace: default/namespace: k10/' | kubectl apply --namespace=k10 -f - || true
+#kubectl create namespace k10
+#kubectl apply -f /Users/bry/sys/etc/k10-oidc-auth.yml
+#kubectl get secret tls-sys --namespace=default -o yaml | sed 's/namespace: default/namespace: k10/' | kubectl apply --namespace=k10 -f - || true
 
 kubectl create clusterrolebinding "kasten-admin--bryan.jiencke@gmail.com" --clusterrole=kasten-admin --user=bryan.jiencke@gmail.com
 kubectl create clusterrolebinding "kasten-admin--bryan.jiencke@gmail.com--cluster-admin" --clusterrole=cluster-admin --user=bryan.jiencke@gmail.com
