f15a99690a05fce955f8aef54d7f8dfd978368df
Bryan Jiencke <bryan.jiencke@gmail.com>
2022-02-20 16:17:40 -0500

    WIP

--- bin/delete-cid-environment.sh
+++ bin/delete-cid-environment.sh
@@ -2,7 +2,7 @@
 
 current_namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}'; echo)
 
-kubectl get ns | grep env-stg | awk '$1 {print$1}' | while read ns; do
+kubectl get ns | grep env-cid | awk '$1 {print$1}' | while read ns; do
   kubectl config set-context --current --namespace=$ns
   helm ls --all --short | xargs -L1 helm delete
   kubectl delete pods --all --force
