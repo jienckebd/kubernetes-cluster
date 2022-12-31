d35753abc241f97469a143d8c7351fa71079b0f0
Bryan Jiencke <bryan.jiencke@gmail.com>
2022-12-31 15:54:59 -0500

    WIP

--- bin/velero-schedule.sh
+++ bin/velero-schedule.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-# velero schedule create admin--1h --schedule="@every 1h" --include-cluster-resources=true --include-namespaces admin --snapshot-volumes=true --default-volumes-to-restic --ttl 24h0m0s && sleep 600
-# velero schedule create env-prd--1h --schedule="@every 1h" --include-namespaces env-prd --snapshot-volumes=true --default-volumes-to-restic --ttl 24h0m0s && sleep 600
-# velero schedule create env-ide1--1h --schedule="@every 1h" --include-namespaces env-ide1 --snapshot-volumes=true --default-volumes-to-restic --ttl 24h0m0s && sleep 600
+# velero schedule create admin--1h --schedule="@every 1h" --include-cluster-resources=true --include-namespaces admin --ttl 24h0m0s && sleep 600
+# velero schedule create env-prd--1h --schedule="@every 1h" --include-namespaces env-prd --ttl 24h0m0s && sleep 600
+# velero schedule create env-ide1--1h --schedule="@every 1h" --include-namespaces env-ide1 --ttl 24h0m0s && sleep 600
 
-velero schedule create admin--24h --schedule="@every 24h" --include-cluster-resources=true --include-namespaces admin --snapshot-volumes=true --default-volumes-to-restic --ttl 720h0m0s && sleep 600
-velero schedule create env-prd--24h --schedule="@every 24h" --include-namespaces env-prd --snapshot-volumes=true --default-volumes-to-restic --ttl 720h0m0s && sleep 600
-velero schedule create gitlab--24h --schedule="@every 24h" --include-namespaces gitlab --snapshot-volumes=true --default-volumes-to-restic --ttl 720h0m0s && sleep 600
-velero schedule create gitlab-agent--24h --schedule="@every 24h" --include-namespaces gitlab-agent --snapshot-volumes=true --default-volumes-to-restic --ttl 720h0m0s && sleep 600
-# velero schedule create env-ide1--24h --schedule="@every 24h" --include-namespaces env-ide1 --snapshot-volumes=true --default-volumes-to-restic --ttl 720h0m0s && sleep 600
+velero schedule create default--24h --schedule="@every 24h" --include-cluster-resources=true --include-namespaces default --ttl 720h0m0s && sleep 600
+velero schedule create admin--24h --schedule="@every 24h" --include-namespaces admin --ttl 720h0m0s && sleep 600
+velero schedule create gitlab--24h --schedule="@every 24h" --include-namespaces gitlab --ttl 720h0m0s && sleep 600
+# velero schedule create env-ide1--24h --schedule="@every 24h" --include-namespaces env-ide1 --ttl 720h0m0s && sleep 600
 
-velero schedule create admin--720h --schedule="@every 720h" --include-cluster-resources=true --include-namespaces admin --snapshot-volumes=true --default-volumes-to-restic --ttl 8760h0m0s && sleep 600
-velero schedule create env-prd--720h --schedule="@every 720h" --include-namespaces env-prd --snapshot-volumes=true --default-volumes-to-restic --ttl 8760h0m0s && sleep 600
-velero schedule create gitlab--720h --schedule="@every 720h" --include-namespaces gitlab --snapshot-volumes=true --default-volumes-to-restic --ttl 8760h0m0s && sleep 600
-velero schedule create gitlab-agent--720h --schedule="@every 720h" --include-namespaces gitlab-agent --snapshot-volumes=true --default-volumes-to-restic --ttl 8760h0m0s && sleep 600
-# velero schedule create env-ide1--720h --schedule="@every 720h" --include-namespaces env-ide1 --snapshot-volumes=true --default-volumes-to-restic --ttl 8760h0m0s && sleep 600
+velero schedule create default--720h --schedule="@every 720h" --include-cluster-resources=true --include-namespaces default --ttl 8760h0m0s && sleep 600
+velero schedule create admin--720h --schedule="@every 720h" --include-namespaces admin --ttl 8760h0m0s && sleep 600
+velero schedule create gitlab--720h --schedule="@every 720h" --include-namespaces gitlab --ttl 8760h0m0s && sleep 600
+# velero schedule create env-ide1--720h --schedule="@every 720h" --include-namespaces env-ide1 --ttl 8760h0m0s && sleep 600
